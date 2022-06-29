b714cf94ab7be1d4f251f7e16e8d2e98cae72d0c
Bình Nguyễn <luubinhdevelopment@gmail.com>
2022-06-29 16:09:28 +0700

    create project laravel 8

--- laravel/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
