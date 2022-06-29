a71820804b942fc2e41f9d6dd76c679c1af50787
Bình Nguyễn <luubinhdevelopment@gmail.com>
2022-06-29 14:45:16 +0700

    edit database and so_do_tt

--- laravel/database/migrations/2022_06_20_153212_create_specialties_table.php
+++ laravel/database/migrations/2022_06_20_153212_create_specialties_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('specialties', function (Blueprint $table) {
+        Schema::create('specialist', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
